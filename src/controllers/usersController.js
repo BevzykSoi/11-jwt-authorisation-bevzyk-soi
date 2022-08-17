e12689cc0ff0240651defa08b5d2e3e78481b403
Bevzyk SOI <bevzyk.soi@gmail.com>
2022-08-17 17:49:45 +0200

    Fixed 17.08

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,9 +1,24 @@
 const { User } = require("../models");
 
+exports.getAll = async (req, res, next) => {
+    try {
+       const allUsers = await User.find();
+
+       res.json(allUsers);
+    } catch (error) {
+       next(error);
+    }
+}
+
 exports.update = async (req, res, next) => {
     try {
         const { id } = req.params;
         const { age, description } = req.body;
+
+        if (age > 150) {
+            res.status(400).send("Age must be under 150!");
+            return;
+        }
 
         if (id !== req.user._id.toString()) {
             res.status(400).send("No access!");
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -5,6 +5,7 @@ const usersController = require("../controllers/usersController");
 
 const router = express.Router();
 
-router.put("/:id", auth, usersController.update)
+router.put("/:id", auth, usersController.update);
+router.get("/", usersController.getAll);
 
 module.exports = router;
